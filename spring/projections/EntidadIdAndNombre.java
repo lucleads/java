1a7d6a650335821669e9dc16192407380bfe1f1b
Lucas Dante <78828405+lucleads@users.noreply.github.com>
2021-05-09 17:38:17 +0200

    Create EntidadIdAndNombre.java

---
+++ spring/projections/EntidadIdAndNombre.java
@@ -0,0 +1,7 @@
+package com.haya.boarding.content.entidad.infrastructure.repository.projections;
+
+/** @author Lucas Dante Elizalde on May, 2021 */
+public interface EntidadIdAndNombre {
+  String getId();
+  String getNombre();
+}
